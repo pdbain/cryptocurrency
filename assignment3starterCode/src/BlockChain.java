0b356aebec2e423d2d566201442cc176771f8f2b
Peter Bain <pdbain@acm.org>
2019-03-10 16:11:13 -0400

    Add block height and get max height block code

--- assignment3starterCode/src/BlockChain.java
+++ assignment3starterCode/src/BlockChain.java
@@ -1,31 +1,76 @@
+import static java.util.Objects.nonNull;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+
 // Block Chain should maintain only limited block nodes to satisfy the functions
 // You should not have all the blocks added to the block chain in memory 
 // as it would cause a memory overflow.
 
 public class BlockChain {
     public static final int CUT_OFF_AGE = 10;
+	private Block root;
+	private final TransactionPool txPool;
 
+	LinkedHashMap<ByteArrayWrapper, Block> chain;
+	
     /**
      * create an empty block chain with just a genesis block. Assume {@code genesisBlock} is a valid
      * block
      */
     public BlockChain(Block genesisBlock) {
         // IMPLEMENT THIS
+        root = genesisBlock;
+        txPool = new TransactionPool();
+        chain = new LinkedHashMap<>();
+        ByteArrayWrapper genesisHash = new ByteArrayWrapper(genesisBlock.getHash());
+		chain.put(genesisHash, genesisBlock);
     }
 
     /** Get the maximum height block */
     public Block getMaxHeightBlock() {
-        // IMPLEMENT THIS
+    	HashMap<Block, Integer> blockHeights = new HashMap<>(chain.size());
+    	int maxHeight = 0;
+    	Block maxBlock = null;
+    	blockHeights.put(root, Integer.valueOf(1));
+    	for (Block b: chain.values()) {
+    		int h = getBlockHeight(b, blockHeights);
+    		if (h > maxHeight) {
+    			maxHeight = h;
+    			maxBlock = b;
+    		}
+    	}
+    	return maxBlock;
     }
 
+    private int getBlockHeight(Block cursor, HashMap<Block, Integer> blockHeights) {
+    	Integer h = blockHeights.get(cursor);
+    	if (nonNull(h)) {
+    		return h.intValue();
+    	} else {
+    		byte[] prevBlockHash = cursor.getPrevBlockHash();
+			Block parent = chain.get(new ByteArrayWrapper(prevBlockHash));
+    		int myHeight = 1 + getBlockHeight(parent, blockHeights);
+    		blockHeights.put(cursor, Integer.valueOf(myHeight));
+    		return myHeight;
+    	}
+    }
+
+     int getBlockHeight(Block cursor) {
+     	HashMap<Block, Integer> blockHeights = new HashMap<>(chain.size());
+     	blockHeights.put(root, Integer.valueOf(1));
+    	return getBlockHeight(cursor, blockHeights);
+    }
     /** Get the UTXOPool for mining a new block on top of max height block */
     public UTXOPool getMaxHeightUTXOPool() {
         // IMPLEMENT THIS
+    	return null; // TODO
     }
-
+    
     /** Get the transaction pool to mine a new block */
     public TransactionPool getTransactionPool() {
         // IMPLEMENT THIS
+        return txPool;
     }
 
     /**
@@ -41,11 +86,24 @@ public class BlockChain {
      * @return true if block is successfully added
      */
     public boolean addBlock(Block block) {
-        // IMPLEMENT THIS
+    	ByteArrayWrapper blockHash = new ByteArrayWrapper(block.getHash());
+    	chain.put(blockHash, block);
+    	return false; // TODO
+
     }
 
     /** Add a transaction to the transaction pool */
     public void addTransaction(Transaction tx) {
         // IMPLEMENT THIS
+        txPool.addTransaction(tx);
+    }
+    
+    public void dumpChain() {
+    	HashMap<Block, Integer> blockHeights = new HashMap<>(chain.size());
+    	blockHeights.put(root, Integer.valueOf(1));
+    	for (Block b: chain.values()) {
+    		int h = getBlockHeight(b, blockHeights);
+    		System.err.println("Block "+b.hashCode()+" height " + h);
+    	}
     }
 }
---
+++ assignment3starterCode/src/TestBlockChain.java
@@ -0,0 +1,118 @@
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.PublicKey;
+import java.util.LinkedHashMap;
+
+import junit.framework.TestCase;
+
+public class TestBlockChain extends TestCase {
+
+	private static final int MAIN_HEIGHT = 6;
+	private static final int SIDE_HEIGHT = 10;
+	private PublicKey mainKey;
+	private Block genesisBlock;
+	private BlockChain chain;
+
+	public void testMakeBlock() throws NoSuchAlgorithmException {
+		Block mainBlock = makeBlock(genesisBlock, mainKey);
+		Block sideBlock = makeBlock(genesisBlock, makePublicKey());
+		LinkedHashMap<ByteArrayWrapper, Block> chain = new LinkedHashMap<>();
+		chain.put(new ByteArrayWrapper(mainBlock.getHash()), mainBlock);
+		chain.put(new ByteArrayWrapper(sideBlock.getHash()), sideBlock);
+		assertEquals(2, chain.size());
+	}
+
+	
+	public void testAddBlocks() throws NoSuchAlgorithmException {
+		// add a linear chain of 8 blocks
+		Block mainChain[] = new Block[MAIN_HEIGHT];
+		mainChain[0] = genesisBlock;
+		assertEquals(1, chain.getBlockHeight(genesisBlock));
+		assertEquals(genesisBlock, chain.getMaxHeightBlock());
+		
+		for (int i = 1; i < MAIN_HEIGHT; ++i) {
+			mainChain[i] = makeBlock(mainChain[i - 1], mainKey);
+		}
+		for (int i = 1; i < MAIN_HEIGHT; ++i) {
+			assertEquals(i + 1, chain.getBlockHeight(mainChain[i]));
+		}
+		assertEquals(mainChain[MAIN_HEIGHT - 1], chain.getMaxHeightBlock());
+		// create a branch off the oldest block
+		Block sideChain[] = new Block[SIDE_HEIGHT];
+		sideChain[0] = genesisBlock;
+		PublicKey sideKey = makePublicKey();
+		for (int i = 1; i < sideChain.length; ++i) {
+			Block newBlock = makeBlock(sideChain[i - 1], sideKey);
+			sideChain[i] = newBlock;
+			assertEquals(i + 1, chain.getBlockHeight(newBlock));
+			if (i < MAIN_HEIGHT) {
+				Block maxHeightBlock = chain.getMaxHeightBlock();
+				assertEquals(mainChain[MAIN_HEIGHT - 1], maxHeightBlock);
+			} else {				
+				assertEquals(newBlock, chain.getMaxHeightBlock());
+			}
+		}
+		assertEquals(sideChain[sideChain.length - 1], chain.getMaxHeightBlock());
+	}
+
+	public void testMidpointBranch() throws NoSuchAlgorithmException {
+		// add a linear chain of 8 blocks
+		Block mainChain[] = new Block[MAIN_HEIGHT];
+		mainChain[0] = genesisBlock;
+		assertEquals(1, chain.getBlockHeight(genesisBlock));
+		assertEquals(genesisBlock, chain.getMaxHeightBlock());
+		
+		for (int i = 1; i < MAIN_HEIGHT; ++i) {
+			mainChain[i] = makeBlock(mainChain[i - 1], mainKey);
+		}
+		for (int i = 1; i < MAIN_HEIGHT; ++i) {
+			assertEquals(i + 1, chain.getBlockHeight(mainChain[i]));
+		}
+		assertEquals(mainChain[MAIN_HEIGHT - 1], chain.getMaxHeightBlock());
+		// create a branch off the oldest block
+		Block sideChain[] = new Block[SIDE_HEIGHT];
+		// create a branch mid chain
+		for (int i = 0; i < 3; ++i) {
+			sideChain[i] = mainChain[i];
+		}
+		PublicKey sideKey = makePublicKey();
+		for (int i = 3; i < sideChain.length; ++i) {
+			Block newBlock = makeBlock(sideChain[i - 1], sideKey);
+			sideChain[i] = newBlock;
+			int h = chain.getBlockHeight(newBlock);
+			assertEquals(i + 1, h);
+			if (h <= MAIN_HEIGHT) {
+				Block maxHeightBlock = chain.getMaxHeightBlock();
+				assertEquals("side block " + i + " height " + h, mainChain[mainChain.length - 1], maxHeightBlock);
+			} else {				
+				assertEquals(newBlock, chain.getMaxHeightBlock());
+			}
+		}
+		assertEquals(sideChain[sideChain.length - 1], chain.getMaxHeightBlock());
+		
+	}
+
+	private Block makeBlock(Block prevBlock, PublicKey theKey) {
+		Block newBlock = new Block(prevBlock.getHash(), theKey);
+		newBlock.addTransaction(newBlock.getCoinbase());
+		newBlock.finalize();
+		chain.addBlock(newBlock);
+		return newBlock;
+	}
+
+	@Override
+	public void setUp() throws NoSuchAlgorithmException {
+		mainKey = makePublicKey();
+		genesisBlock = new Block(null, mainKey);
+		genesisBlock.finalize();
+		chain = new BlockChain(genesisBlock);
+	}
+
+	private PublicKey makePublicKey() throws NoSuchAlgorithmException {
+		KeyPairGenerator kpg = KeyPairGenerator.getInstance("RSA");
+		KeyPair keyPair = kpg.generateKeyPair();
+		PublicKey public1 = keyPair.getPublic();
+		return public1;
+	}
+}
